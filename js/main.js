5e49aefc97f15035dfd554f7c15cff368cf5a00e
ShakirovaAliya <60111146+Shika8users.noreply.github.com>
2021-02-01 17:21:33 +0300

    корректировка оформления

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ let getRandomIntInclusive = function(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
-  } else {return undefined}
+  } else {
+    return null
+  }
 }
+getRandomIntInclusive(2, 180);
 
-console.log(getRandomIntInclusive(2, 180));
 
 // Функция для проверки максимальной длины строки
 
@@ -16,14 +18,13 @@ let commentField = document.querySelector('.social__footer-text');
 const MAX_COMMENT_LENGTH = 140;
 
 let checkCommentLenght = function(comment, maxLenght) {
-if(comment.maxlength<=maxLenght) {
-  return 'true';
-} else { return 'false'}
+  if(comment.maxlength<=maxLenght) {
+    return 'true';
+  } else {
+    return 'false'
+  }
 }
 
 commentField.maxlength = 160;
+checkCommentLenght(commentField, MAX_COMMENT_LENGTH);
 
-console.log(checkCommentLenght(commentField, MAX_COMMENT_LENGTH));
-
-// имя_функции(проверяемая_строка, максимальная длина);
-// Результат: true, если строка проходит по длине, и false — если не проходит
