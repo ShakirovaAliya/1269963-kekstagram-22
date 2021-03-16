6c802714b9c6bb9007cb3abd9bb5a63815c8a867
ShakirovaAliya <60111146+Shika8users.noreply.github.com>
2021-03-16 22:47:20 +0300

    попытка сделать фильтр

--- js/create-photo.js
+++ js/create-photo.js
@@ -1,13 +1,11 @@
-//  создание массива из 25 сгенерированных объектов
-
-// import { getRandomIntInclusive } from './util.js';
 import { createBigPicture } from './big-photo.js';
 
+// создание массива из 25 сгенерированных объектов
 const PHOTO_COUNT = 25;
 const PHOTO_RANDOM_COUNT = 10;
-// const namesArray = ['Артем', 'Иван', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
 /*
+const namesArray = ['Артем', 'Иван', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 let createPhoto = function (count) {
   let photoArray = [];
   let commentsArray = [];
@@ -34,76 +32,30 @@ let createPhoto = function (count) {
   }
   return photoArray;
 };
+const createPhotos = () => new Array(PHOTO_COUNT).fill(null).map(() => createPhoto());
+const similarPhotos = createPhotos();
 */
 
-// const createPhotos = () => new Array(PHOTO_COUNT).fill(null).map(() => createPhoto());
-// const similarPhotos = createPhotos();
 const similarPictureBlock = document.querySelector('.pictures');
 const similarPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-let imgFilter = document.querySelector('.img-filters');
-let filterButtonDefault = imgFilter.querySelector('#filter-default');
-let filterButtonRandom = imgFilter.querySelector('#filter-random');
-let filterButtonDiscussed = imgFilter.querySelector('#filter-discussed');
-
-
-const clearSimilarList = () => {
-  let picturesElement = document.querySelectorAll('.picture');
-  picturesElement.forEach((element) => {
-    element.innerHTML = '';
-  })
-};
 
 const renderSimilarList = (similarPhotos) => {
   const similarPhotoFragment = document.createDocumentFragment();
 
-  similarPhotos
+  similarPhotos.slice()
     .forEach((foto) => {
       const pictureElement = similarPictureTemplate.cloneNode(true);
       pictureElement.querySelector('.picture__img').src = foto.url;
       pictureElement.querySelector('.picture__likes').textContent = foto.likes;
       pictureElement.querySelector('.picture__comments').textContent = foto.comments.length;
       // pictureElement.dataset.id = foto.id;
+      similarPhotoFragment.innerHTML = '';
       similarPhotoFragment.appendChild(pictureElement);
       pictureElement.addEventListener('click', function () {
         createBigPicture(foto);
       });
     });
-  clearSimilarList();
   similarPictureBlock.appendChild(similarPhotoFragment);
-  //console.log(similarPictureBlock);
-}
-
-let setFilterDefault = function () {
-  filterButtonDefault.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    if (!filterButtonDefault.classList.contains('img-filters__button--active')) {
-      filterButtonDefault.classList.add('img-filters__button--active');
-      filterButtonRandom.classList.remove('img-filters__button--active');
-      filterButtonDiscussed.classList.remove('img-filters__button--active');
-    }
-  })
-}
-
-let setFilterRandom = function () {
-  filterButtonRandom.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    if (!filterButtonRandom.classList.contains('img-filters__button--active')) {
-      filterButtonRandom.classList.add('img-filters__button--active');
-      filterButtonDefault.classList.remove('img-filters__button--active');
-      filterButtonDiscussed.classList.remove('img-filters__button--active');
-    }
-  })
-}
-
-let setFilterDiscussed = function () {
-  filterButtonDiscussed.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    if (!filterButtonDiscussed.classList.contains('img-filters__button--active')) {
-      filterButtonDiscussed.classList.add('img-filters__button--active');
-      filterButtonDefault.classList.remove('img-filters__button--active');
-      filterButtonRandom.classList.remove('img-filters__button--active');
-    }
-  })
 }
 
 let sortPhoto = function (a, b) {
@@ -115,4 +67,5 @@ let sortPhoto = function (a, b) {
   }
   return 0;
 };
-export { renderSimilarList, PHOTO_COUNT, PHOTO_RANDOM_COUNT, imgFilter, setFilterDefault, setFilterRandom, setFilterDiscussed, sortPhoto }
+
+export { renderSimilarList, PHOTO_COUNT, PHOTO_RANDOM_COUNT, sortPhoto }
--- js/main.js
+++ js/main.js
@@ -1,16 +1,63 @@
-import { renderSimilarList, PHOTO_COUNT, PHOTO_RANDOM_COUNT, imgFilter, setFilterDefault, setFilterRandom, setFilterDiscussed, sortPhoto } from './create-photo.js';
+import { renderSimilarList, PHOTO_COUNT, PHOTO_RANDOM_COUNT, sortPhoto } from './create-photo.js';
 import { setFormSubmit, toCloseForm } from './form.js';
 import { getData, toShowFailMessage } from './api.js';
 // import './photo-correction.js';
 
-getData(
-  (allFotos) => { renderSimilarList(allFotos.slice(0, PHOTO_COUNT)) },
-  (err) => { toShowFailMessage(err) },
-  imgFilter.classList.remove('img-filters--inactive'),
-)
+let imgFilter = document.querySelector('.img-filters');
+let filterButtonDefault = imgFilter.querySelector('#filter-default');
+let filterButtonRandom = imgFilter.querySelector('#filter-random');
+let filterButtonDiscussed = imgFilter.querySelector('#filter-discussed');
+filterButtonDefault.classList.remove('img-filters__button--active');
+const similarPictureBlock = document.querySelector('.pictures');
 
-//renderSimilarList(allFotos.slice(0, PHOTO_COUNT))); // фильтр по умолчанию
-//renderSimilarList(allFotos.slice(0, PHOTO_RANDOM_COUNT))); // рандомный фильтр 10 фото
-//renderSimilarList(allFotos.sort((sortPhoto)))); // фильтр по кол-ву комментариев
+getData(
+  (allFotos) => {
+    renderSimilarList(allFotos);
+    imgFilter.classList.remove('img-filters--inactive');
+
+    filterButtonDefault.addEventListener('click', function () {
+      similarPictureBlock.innerHTML = '';
+      if (!filterButtonDefault.classList.contains('img-filters__button--active')) {
+        filterButtonDefault.classList.add('img-filters__button--active');
+        filterButtonRandom.classList.remove('img-filters__button--active');
+        filterButtonDiscussed.classList.remove('img-filters__button--active');
+      } else {
+        filterButtonDefault.classList.remove('img-filters__button--active');
+      }
+      renderSimilarList(allFotos.slice(0, PHOTO_COUNT));
+    });
+    filterButtonRandom.addEventListener('click', function () {
+      similarPictureBlock.innerHTML = '';
+      if (!filterButtonRandom.classList.contains('img-filters__button--active')) {
+        filterButtonRandom.classList.add('img-filters__button--active');
+        filterButtonDefault.classList.remove('img-filters__button--active');
+        filterButtonDiscussed.classList.remove('img-filters__button--active');
+      } else {
+        filterButtonRandom.classList.remove('img-filters__button--active');
+      }
+      renderSimilarList(allFotos.slice(0, PHOTO_RANDOM_COUNT));
+    });
+    filterButtonDiscussed.addEventListener('click', function () {
+      similarPictureBlock.innerHTML = '';
+      if (!filterButtonDiscussed.classList.contains('img-filters__button--active')) {
+        filterButtonDiscussed.classList.add('img-filters__button--active');
+        filterButtonDefault.classList.remove('img-filters__button--active');
+        filterButtonRandom.classList.remove('img-filters__button--active');
+      } else {
+        filterButtonDiscussed.classList.remove('img-filters__button--active');
+      }
+      renderSimilarList(allFotos.sort(sortPhoto).slice(0, PHOTO_COUNT));
+    });
+    if (!filterButtonDiscussed.classList.contains('img-filters__button--active') && !filterButtonRandom.classList.contains('img-filters__button--active') && !filterButtonDefault.classList.contains('img-filters__button--active')) {
+      renderSimilarList(allFotos.slice(0, PHOTO_COUNT));
+    }
+  },
+  (err) => { toShowFailMessage(err) },
+);
+
+
+// renderSimilarList(allFotos.slice(0, PHOTO_COUNT)); // фильтр по умолчанию
+// renderSimilarList(allFotos.slice(0, PHOTO_RANDOM_COUNT)) рандомный фильтр 10 фото
+// renderSimilarList(allFotos.sort(sortPhoto).slice(0, PHOTO_COUNT); // фильтр по кол-ву комментариев
 
 setFormSubmit(toCloseForm);
