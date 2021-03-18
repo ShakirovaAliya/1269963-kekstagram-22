d131c55ed988bf8b51412d63d45b32d12bd0a673
ShakirovaAliya <60111146+Shika8users.noreply.github.com>
2021-03-18 16:55:57 +0300

    исправления

--- js/create-photo.js
+++ js/create-photo.js
@@ -3,39 +3,6 @@ import { createBigPicture } from './big-photo.js';
 // создание массива из 25 сгенерированных объектов
 const PHOTO_COUNT = 25;
 const PHOTO_RANDOM_COUNT = 10;
-
-/*
-const namesArray = ['Артем', 'Иван', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-let createPhoto = function (count) {
-  let photoArray = [];
-  let commentsArray = [];
-  let nameIndex = getRandomIntInclusive(0, namesArray.length - 1);
-  let avatarIndex = getRandomIntInclusive(1, 6);
-  let photoUrlIndex = getRandomIntInclusive(1, 25);
-  for (let j = 0; j < 50; j++) {
-    commentsArray.push({
-      id: getRandomIntInclusive(1, 1000),
-      avatar: 'img/avatar-' + avatarIndex + '.svg',
-      message: 'Всё отлично!',
-      name: namesArray[nameIndex],
-    })
-  }
-  for (let i = 0; i < count; i++) {
-    photoArray.push({
-      id: getRandomIntInclusive(1, 25),
-      url: 'photos/' + photoUrlIndex + '.jpg',
-      description: 'В целом всё неплохо. Но не всё.',
-      likes: getRandomIntInclusive(15, 200),
-      comments: commentsArray,
-    })
-
-  }
-  return photoArray;
-};
-const createPhotos = () => new Array(PHOTO_COUNT).fill(null).map(() => createPhoto());
-const similarPhotos = createPhotos();
-*/
-
 const similarPictureBlock = document.querySelector('.pictures');
 const similarPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
@@ -49,7 +16,7 @@ const renderSimilarList = (similarPhotos) => {
       pictureElement.querySelector('.picture__likes').textContent = foto.likes;
       pictureElement.querySelector('.picture__comments').textContent = foto.comments.length;
       // pictureElement.dataset.id = foto.id;
-      similarPhotoFragment.innerHTML = '';
+      //similarPhotoFragment.innerHTML = '';
       similarPhotoFragment.appendChild(pictureElement);
       pictureElement.addEventListener('click', function () {
         createBigPicture(foto);
